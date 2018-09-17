202a124036d9879d093f5d1c1f40aa174ec8dd7a
Chris Golding <cgolding@vbgov.com>
2018-09-17 10:24:44 -0400

    Initial Commit

---
+++ YouTubePlayer iOS 12 Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YouTubePlayer iOS 12 Test
+//
+//  Created by Chris Golding on 9/17/18.
+//  Copyright © 2018 City of Virginia Beach. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
